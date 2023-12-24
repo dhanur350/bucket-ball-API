44a7b1ee920c6882330778a9e9ab0a459d419e48
dhanur350 <dhanur297@gmail.com>
2023-12-24 13:05:18 +0530

    API config setting

---
+++ bucketBall/connections/index.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const { MongoClient } = require("mongodb");
+const { database } = require("../../config");
+
+const client = new MongoClient(database);
+
+async function configDatabase() {
+  const connect = await client.connect();
+  const dbInstance = connect.db(process.env.BUCKET_DB);
+  const collection = dbInstance.collection(process.env)
+}
