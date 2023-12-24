1565efe52b94a1598b35c8af01fc4a975f61180a
dhanur350 <dhanur297@gmail.com>
2023-12-24 22:16:54 +0530

    CORS FIX

--- client/index.js
+++ client/index.js
@@ -1,10 +1,43 @@
-const form = document.getElementById('bucket-form');
+const bucketForm = document.getElementById("bucket-form");
+const ballForm = document.getElementById("ball-form");
+const bucketSuggestions = document.getElementById("bucket-suggestions");
 
-form.addEventListener('submit',async (event)=> {
+bucketForm.addEventListener("submit", async (event) => {
   event.preventDefault();
-  const submittedData = new FormData(form);
+  const submittedData = new FormData(bucketForm);
   const formDataObject = Object.fromEntries(submittedData);
   console.log(formDataObject);
-})
 
+  try {
+    const response = await fetch("http://localhost:4000/bucketList", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formDataObject),
+    });
 
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+});
+
+ballForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const submittedData = new FormData(ballForm);
+  const formDataObject = Object.fromEntries(submittedData);
+  console.log(formDataObject);
+});
+
+bucketSuggestions.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const submittedData = new FormData(bucketSuggestions);
+  const formDataObject = Object.fromEntries(submittedData);
+  console.log(formDataObject);
+});
--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const bodyparser = require('body-parser')
 const cors = require("cors");
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT;
 
+app.use(bodyparser.urlencoded({extended:true}))
+app.use(bodyparser.json())
 app.use(express.json());
 
 app.use(
@@ -14,6 +17,13 @@ app.use(
 );
 
 app.get("/", () => {});
+
+app.post('/bucketList',(req,res)=> {
+  const data = req.body;
+  console.log(data);
+  res.json(data);
+  res.end();
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
